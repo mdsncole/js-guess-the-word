08fd2877b3782ec770e370e646a6bc39ce6753b8
madison <mdsncole97@gmail.com>
2023-08-08 15:29:12 -0500

    Created global variables

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// GLOBAL VARIABLES
+
+const ul = document.querySelector("ul");
+const guess = document.querySelector(".guess");
+const letter = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remaining = document.querySelector(".remaining");
+const letterGuessed = document.querySelector(".guessed-letter");
+const playAgain = document.querySelector(".play-again");
+
+const word = "magnolia";
