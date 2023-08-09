7f183eddb56b5b27d8cc24c86458930c39973d1b
madison <mdsncole97@gmail.com>
2023-08-08 19:32:30 -0500

    Added placeholders for each letter, added event listener to button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 // GLOBAL VARIABLES
 
-const ul = document.querySelector("ul");
-const guess = document.querySelector(".guess");
+const guessButton = document.querySelector(".guess");
 const letter = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remaining = document.querySelector(".remaining");
-const letterGuessed = document.querySelector(".guessed-letter");
+const remainingSpan = document.querySelector(".remaining span");
+const guessedLetter = document.querySelector(".guessed-letters");
+const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+}
+
+placeholder(word);
+
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = letter.value;
+    console.log(guess);
+    letter.value = "";
+});
